d1bb5675f9bba9896ff418fa95b94ae04b89fabc
WillSchmidt97 <willschmidt1997@gmail.com>
2024-01-28 13:18:31 -0300

    select in

--- Program.cs
+++ Program.cs
@@ -265,4 +265,18 @@ class Program {
             }
         }
     }
+
+    static void SelectIn(SqlConnection connection) {
+        var query = @"SELECT * FROM [Career] WHERE [Id] IN @Id";
+
+        var items = connection.Query<Career>(query, new {
+                    Id = new[] {
+                        "4327ac7e-963b-4893-9f31-9a3b28a4e72b",
+                        "e6730d1c-6870-4df3-ae68-438624e04c72"
+                    }
+                });
+        foreach (var item in items) {
+            Console.WriteLine($"{item.Title}");
+        }
+    }
 }
