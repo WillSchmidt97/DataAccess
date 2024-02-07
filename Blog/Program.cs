9f309e4fae9bc47d0bac85ba4b4e2a3e0ac682d3
WillSchmidt97 <willschmidt1997@gmail.com>
2024-02-07 00:31:40 -0300

    starting the project

---
+++ Blog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string ConnectionString = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ReadUsers()
+        {
+            using(var connection = new SqlConnection(ConnectionString)) {
+                var users = connection.GetAll<User>();
+
+                foreach(var user in users) {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}	
--- Models/CareerItem.cs
+++
@@ -1,9 +0,0 @@
-namespace Models
-{
-    public class CareerItem
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public Course Course { get; set; }
-    }
-}
