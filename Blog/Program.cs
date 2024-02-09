71b787a66a1b0f07edf283b63c1bc2cba44af3b3
WillSchmidt97 <willschmidt1997@gmail.com>
2024-02-09 01:30:31 -0300

    CRUD

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -23,5 +23,54 @@ namespace Blog
                 }
             }
         }
+        public static void ReadUser()
+        {
+            using(var connection = new SqlConnection(ConnectionString)) {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+        public static void CreateUser()
+        {
+            var user = new User() {
+                Bio = "I'm a software developer",
+                Email = "bill.schmidt20@gmail.com",
+                Image = "https://www.google.com",
+                Name = "Bill Schmidt",
+                PasswordHash = "HASH",
+                Slug = "bill-schmidt"
+            };
+
+            using(var connection = new SqlConnection(ConnectionString)) {
+                connection.Insert<User>(user);
+                Console.WriteLine($"User {user.Name} has been created");
+            }
+        }
+        
+        public static void UpdateUser()
+        {
+            var user = new User() {
+                Id = 2,
+                Bio = "I'm a backend developer",
+                Email = "bill.schmidt20@gmail.com",
+                Image = "https://www.google.com",
+                Name = "William Schmidt",
+                PasswordHash = "HASH",
+                Slug = "will-schmidt"
+            };
+
+            using(var connection = new SqlConnection(ConnectionString)) {
+                connection.Update<User>(user);
+                Console.WriteLine($"User {user.Name} has been updated");
+            }
+        }   
+        public static void DeleteUser()
+        {
+            using(var connection = new SqlConnection(ConnectionString)) {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine($"User {user.Name} has been deleted");
+            }
+        }               
     }
 }	
