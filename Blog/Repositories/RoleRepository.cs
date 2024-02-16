5f1efc43d51c70136c0c5bd6f2230624f34d4266
WillSchmidt97 <willschmidt1997@gmail.com>
2024-02-15 23:30:53 -0300

    optimizing repository

--- Blog/Repositories/RoleRepository.cs
+++ Blog/Repositories/RoleRepository.cs
@@ -17,5 +17,26 @@ namespace Blog.Repositories
         public Role Get(int id) => _connection.Get<Role>(id);
 
         public void Create(Role role) => _connection.Insert<Role>(role);
+
+        public void Update(Role role) 
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+        }
+
+        public void Delete(int id) 
+        {
+            if (id != 0)
+                return;
+            
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }	
+
+        public void Delete(Role role) 
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+        }
     }
 }
