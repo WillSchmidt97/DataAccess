f37e20bb331929f6c6d2e363174eb004ff9b421c
WillSchmidt97 <willschmidt1997@gmail.com>
2024-03-03 01:16:19 -0300

    adding other CRUD operations

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -13,12 +13,16 @@ namespace Blog
         {
             var connection = new SqlConnection(ConnectionString);
             connection.Open();
-            ReadUsers(connection);
-            ReadRoles(connection);
-            ReadTags(connection);
-           // CreateUser();
-           // UpdateUser();
-           // DeleteUser();
+            //ReadUsers(connection);
+            //ReadRoles(connection);
+            //ReadTags(connection);
+            //CreateUser(connection);
+            //CreateRole(connection);
+            //CreateTag(connection);
+            UpdateUser(connection);
+            //UpdateRole(connection);
+            //UpdateTag(connection);
+            //DeleteUser(connection);
             connection.Close();
         }
 
@@ -29,7 +33,64 @@ namespace Blog
 
             foreach(var user in users) 
                 Console.WriteLine(user.Name);
+        }
+
+        public static void ReadUser(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<User>(sqlConnection);
+            var user = repository.Get(1);
+
+            Console.WriteLine(user.Name);
+        }
+
+        public static void CreateUser(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<User>(sqlConnection);
+
+            repository.Create(new User
+            {
+                Name = "William Schmidt",
+                Email = "billy.schmidt1997",
+                PasswordHash = "123456",
+                Bio = "I'm a software developer",
+                Image = "https://randomuser.me/api/portraits",
+                Slug = "bill-schmidt"
+            });
+
+            var newUser = repository.GetAll().Last();
+            Console.WriteLine($"New user: {newUser.Name}");
         }      
+
+        public static void UpdateUser(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<User>(sqlConnection);
+            var user = repository.Get(6);
+            if (user != null) {
+                user.Name = "Bill Schmidt";
+                user.Email = "willschmidt1997@gmail.com";
+                user.Bio = "I'm a .NET developer";
+                repository.Update(user);
+
+                Console.WriteLine($"User {user.Name} updated");
+            }
+            else
+                Console.WriteLine("User not found");
+        }
+
+        public static void DeleteUser(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<User>(sqlConnection);
+            var user = repository.Get(6);
+            if (user != null) {
+                repository.Delete(user);
+
+                Console.WriteLine("User deleted");
+            }
+            else
+            {
+                Console.WriteLine("User not found");
+            }
+        }
 
         public static void ReadRoles(SqlConnection sqlConnection)
         {
@@ -40,6 +101,58 @@ namespace Blog
                 Console.WriteLine(item.Name);
         }
 
+        public static void ReadRole(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Role>(sqlConnection);
+            var item = repository.Get(1);
+
+            Console.WriteLine(item.Name);
+        }
+
+        public static void CreateRole(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Role>(sqlConnection);
+
+            repository.Create(new Role
+            {
+                Name = "Administrator",
+                Slug = "main-administrator"
+            });
+
+            var newRole = repository.GetAll().Last();
+            Console.WriteLine($"New role: {newRole.Name}");
+        }
+
+        public static void UpdateRole(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Role>(sqlConnection);
+            var item = repository.Get(1);
+            if (item != null) {
+                item.Name = "Admin";
+                item.Slug = "second-admin";
+                repository.Update(item);
+
+                Console.WriteLine($"Role {item.Name} updated");
+            }
+            else
+                Console.WriteLine("Role not found");
+        }
+
+        public static void DeleteRole(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Role>(sqlConnection);
+            var item = repository.Get(1);
+            if (item != null) {
+                repository.Delete(item);
+
+                Console.WriteLine("Role deleted");
+            }
+            else
+            {
+                Console.WriteLine("Role not found");
+            }
+        }
+
         public static void ReadTags(SqlConnection sqlConnection)
         {
             var repository = new Repository<Tag>(sqlConnection);
@@ -47,6 +160,60 @@ namespace Blog
 
             foreach(var tag in tags) 
                 Console.WriteLine(tag.Name);
-        }        
+        }
+
+        public static void ReadTag(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Tag>(sqlConnection);
+            var tag = repository.Get(1);
+
+            Console.WriteLine(tag.Name);
+        }
+
+        public static void CreateTag(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Tag>(sqlConnection);
+
+            repository.Create(new Tag
+            {
+                Name = "C#",
+                Slug = "c-sharp"
+            });
+
+            var newTag = repository.GetAll().Last();
+            Console.WriteLine($"New tag: {newTag.Name}");
+        }
+
+        public static void UpdateTag(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Tag>(sqlConnection);
+            var tag = repository.Get(1);
+
+            if (tag != null) {
+                tag.Name = "C# 9";
+                tag.Slug = "c-sharp-9";
+                repository.Update(tag);
+
+                Console.WriteLine($"Tag {tag.Name} updated");
+            }
+            else
+                Console.WriteLine("Tag not found");
+        }
+
+        public static void DeleteTag(SqlConnection sqlConnection)
+        {
+            var repository = new Repository<Tag>(sqlConnection);
+            var tag = repository.Get(1);
+
+            if (tag != null) {
+                repository.Delete(tag);
+
+                Console.WriteLine("Tag deleted");
+            }
+            else
+            {
+                Console.WriteLine("Tag not found");
+            }
+        }	        
     }
 }	
